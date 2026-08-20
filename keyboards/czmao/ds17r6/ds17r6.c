64b7c67e0e63b0347c9a8743fd3a8a297c3f9b36
Micah <45813290+micahyy@users.noreply.github.com>
2026-08-20 22:11:54 +0800

    Use ChibiOS PAL functions for B13 init

--- keyboards/czmao/ds17r6/ds17r6.c
+++ keyboards/czmao/ds17r6/ds17r6.c
@@ -1,16 +1,16 @@
 #include "quantum.h"
-#include "gpio.h"
 #include "wait.h"
 
-// WS2812 data line reset: hold B13 low for >280us to reset LED strip,
-// preventing random green LED on plug-in.
+// WS2812 data line reset on power-up: hold B13 low for >280us to reset
+// the LED strip, preventing random green LED on plug-in.
 void keyboard_pre_init_kb(void) {
-    setPinOutput(B13);
-    writePinLow(B13);
+    palSetLineMode(B13, PAL_MODE_OUTPUT_PUSHPULL);
+    palClearLine(B13);
     wait_us(300);
     keyboard_pre_init_user();
 }
 
+// Ensure all RGB LEDs are off at startup
 void matrix_init_kb(void) {
     rgb_matrix_set_color_all(0, 0, 0);
     matrix_init_user();

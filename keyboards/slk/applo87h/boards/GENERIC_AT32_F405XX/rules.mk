34c1561c368dc4a34cba5ff1b33b33560e55f3d9
Micah <45813290+micahyy@users.noreply.github.com>
2026-08-21 01:31:47 +0800

    Remove board rules.mk (MCU now handled by mcu_selection.mk)

--- keyboards/slk/applo87h/boards/GENERIC_AT32_F405XX/rules.mk
+++
@@ -1,8 +0,0 @@
-# Board-level overrides for AT32F405 (applied after mcu_selection.mk).
-# QMK's mcu_selection only knows AT32F415, but the actual chip is AT32F405.
-# We override MCU_SERIES to use the AT32F402_405 HAL port which correctly
-# registers OTG2/HS support for the AT32F405 high-speed USB peripheral.
-
-MCU_SERIES   = AT32F402_405
-MCU_STARTUP  = at32f405
-MCU_LDSCRIPT = AT32F405xC

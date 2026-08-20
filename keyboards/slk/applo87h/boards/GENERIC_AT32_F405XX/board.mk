74dd87690e0b9e003dbc3e7d5853db1c2bb2ebd0
Micah <45813290+micahyy@users.noreply.github.com>
2026-08-21 00:40:26 +0800

    Fix board structure: board.mk at root, board.c/h in board/ subdir

--- keyboards/slk/applo87h/boards/GENERIC_AT32_F405XX/board.mk
+++ keyboards/slk/applo87h/boards/GENERIC_AT32_F405XX/board.mk
@@ -1,8 +1,8 @@
 # List of all the board related files.
-BOARDSRC = $(BOARD_PATH)/board.c
+BOARDSRC = $(BOARD_PATH)/board/board.c
 
 # Required include directories
-BOARDINC = $(BOARD_PATH)
+BOARDINC = $(BOARD_PATH)/board
 
 # Shared variables
 ALLCSRC += $(BOARDSRC)

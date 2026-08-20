5c7a7e1752ffa4817a37761b27a59f538f5dff22
Micah <45813290+micahyy@users.noreply.github.com>
2026-08-20 22:22:54 +0800

    via: use new RM_* rgb_matrix keycodes

--- keyboards/czmao/ds17r6/keymaps/via/keymap.c
+++ keyboards/czmao/ds17r6/keymaps/via/keymap.c
@@ -9,11 +9,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P0,                 KC_PDOT, KC_PENT
     ),
     [1] = LAYOUT_numberpad_5x4(
-        KC_TRNS,        KC_TRNS,     MO(2),          RGB_VAI,
-        RGB_TOG,        RGB_SAI,     KC_TRNS,
-        RGB_HUD,        KC_TRNS,     RGB_HUI,        RGB_VAD,
-        KC_TRNS,        RGB_SAD,     KC_TRNS,
-        RGB_MODE_FORWARD,           KC_TRNS,        KC_TRNS
+        KC_TRNS, KC_TRNS, MO(2),   RM_VALU,
+        RM_TOGG, RM_SATU, KC_TRNS,
+        RM_HUED, KC_TRNS, RM_HUEU, RM_VALD,
+        KC_TRNS, RM_SATD, KC_TRNS,
+        RM_NEXT,          KC_TRNS, KC_TRNS
     ),
     [2] = LAYOUT_numberpad_5x4(
         KC_TRNS, KC_TRNS, KC_TRNS, NK_TOGG,

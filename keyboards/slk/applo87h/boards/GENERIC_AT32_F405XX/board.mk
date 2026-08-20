7f0715c1037abff93764c9b3fc2c85576efd4984
Micah <45813290+micahyy@users.noreply.github.com>
2026-08-21 00:51:38 +0800

    Add configs to BOARDINC for mcuconf.h

--- keyboards/slk/applo87h/boards/GENERIC_AT32_F405XX/board.mk
+++ keyboards/slk/applo87h/boards/GENERIC_AT32_F405XX/board.mk
@@ -1,8 +1,8 @@
 # List of all the board related files.
 BOARDSRC = $(BOARD_PATH)/boards/GENERIC_AT32_F405XX/board.c
 
-# Required include directories
-BOARDINC = $(BOARD_PATH)/boards/GENERIC_AT32_F405XX
+# Required include directories (board files + configs for mcuconf.h)
+BOARDINC = $(BOARD_PATH)/boards/GENERIC_AT32_F405XX $(BOARD_PATH)/boards/GENERIC_AT32_F405XX/configs
 
 # Shared variables
 ALLCSRC += $(BOARDSRC)

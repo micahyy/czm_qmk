33e3fdd5cdb222c5c93c35d68336e0a9be800da7
Micah <45813290+micahyy@users.noreply.github.com>
2026-08-21 10:46:06 +0800

    fix: rewrite board.h based on Keychron AT32F405 reference - fix GPIOC pull, PA11/12 floating, proper init

--- keyboards/slk/applo87h/boards/GENERIC_AT32_F405XX/board.h
+++ keyboards/slk/applo87h/boards/GENERIC_AT32_F405XX/board.h
@@ -22,13 +22,21 @@
 #define AT32_HEXTCLK                12000000
 #endif
 
+/* AT32F405RC: LQFP64, 256KB Flash, 96KB SRAM */
 #define AT32F405RC
 
-/* AT32F405 OTGHS uses the on-chip high-speed PHY; this macro selects the HS clock/path in ChibiOS. */
+/* AT32F405RC has GPIOA, GPIOB, GPIOC, GPIOD, GPIOF */
+#define AT32_HAS_GPIOA              TRUE
+#define AT32_HAS_GPIOB              TRUE
+#define AT32_HAS_GPIOC              TRUE
+#define AT32_HAS_GPIOD              TRUE
+#define AT32_HAS_GPIOF              TRUE
+
+/* OTGHS uses on-chip HS PHY; this macro selects HS clock/path in ChibiOS */
 #define BOARD_OTG2_USES_ULPI
 
 /*
- * IO pin assignments. Values are only used by VAL_* macros below.
+ * IO pins assignments.
  */
 #define GPIOA_PIN0                  0U
 #define GPIOA_PIN1                  1U
@@ -61,8 +69,6 @@
 #define GPIOB_PIN11                 11U
 #define GPIOB_PIN12                 12U
 #define GPIOB_USB_VBUS              13U
-#define GPIOB_PIN14                 14U
-#define GPIOB_PIN15                 15U
 
 #define GPIOC_PIN0                  0U
 #define GPIOC_PIN1                  1U
@@ -81,14 +87,12 @@
 #define GPIOC_PIN14                 14U
 #define GPIOC_PIN15                 15U
 
-#define GPIOD_PIN0                  0U
-#define GPIOD_PIN1                  1U
-#define GPIOD_LED_SCROLL            2U
+#define GPIOD_PIN2                  2U
 
 #define GPIOF_HEXT_IN               0U
 #define GPIOF_HEXT_OUT              1U
 
-/* Input/mode/value macros matching AT32 F402/F405 GPIOv2. */
+/* PIN helper macros matching AT32 F402/F405 GPIOv2 registers */
 #define PIN_MODE_INPUT(n)           (0U << ((n) * 2U))
 #define PIN_MODE_OUTPUT(n)          (1U << ((n) * 2U))
 #define PIN_MODE_MUX(n)             (2U << ((n) * 2U))
@@ -107,73 +111,142 @@
 #define PIN_HDRV_ENABLED(n)         (1U << (n))
 
 /*
- * Safe reset defaults. Matrix rows/columns and LEDs are reconfigured by QMK.
- * PA13/PA14 keep SWD; PF0/PF1 keep HEXT; PB13 is VBUS sense input.
+ * Port A setup.
+ * All pins input pull-up except PA11/PA12 (OTGFS, floating).
+ * PA13/PA14 are SWD (mux), but QMK matrix_init reconfigures PA14 as col input.
  */
-#define VAL_GPIOA_CFGR (PIN_MODE_INPUT(GPIOA_PIN0) | PIN_MODE_INPUT(GPIOA_PIN1) | \
-  PIN_MODE_INPUT(GPIOA_LED_CAPS) | PIN_MODE_INPUT(GPIOA_PIN3) | PIN_MODE_INPUT(GPIOA_PIN4) | \
-  PIN_MODE_INPUT(GPIOA_PIN5) | PIN_MODE_INPUT(GPIOA_PIN6) | PIN_MODE_INPUT(GPIOA_PIN7) | \
-  PIN_MODE_INPUT(GPIOA_PIN8) | PIN_MODE_INPUT(GPIOA_PIN9) | PIN_MODE_INPUT(GPIOA_PIN10) | \
-  PIN_MODE_INPUT(GPIOA_PIN11) | PIN_MODE_INPUT(GPIOA_PIN12) | PIN_MODE_MUX(GPIOA_SWDIO) | \
-  PIN_MODE_MUX(GPIOA_SWCLK) | PIN_MODE_INPUT(GPIOA_PIN15))
-#define VAL_GPIOA_OMODE 0
-#define VAL_GPIOA_ODRVR 0
-#define VAL_GPIOA_PULL (PIN_PULL_PULLUP(GPIOA_PIN0) | PIN_PULL_PULLUP(GPIOA_PIN1) | \
-  PIN_PULL_PULLUP(GPIOA_LED_CAPS) | PIN_PULL_PULLUP(GPIOA_PIN3) | PIN_PULL_PULLUP(GPIOA_PIN4) | \
-  PIN_PULL_PULLUP(GPIOA_PIN5) | PIN_PULL_PULLUP(GPIOA_PIN6) | PIN_PULL_PULLUP(GPIOA_PIN7) | \
-  PIN_PULL_PULLUP(GPIOA_PIN8) | PIN_PULL_PULLUP(GPIOA_PIN9) | PIN_PULL_PULLUP(GPIOA_PIN10) | \
-  PIN_PULL_PULLUP(GPIOA_PIN11) | PIN_PULL_PULLUP(GPIOA_PIN12) | PIN_PULL_PULLUP(GPIOA_SWDIO) | \
-  PIN_PULL_PULLDOWN(GPIOA_SWCLK) | PIN_PULL_PULLUP(GPIOA_PIN15))
-#define VAL_GPIOA_ODT 0xFFFFFFFFU
-#define VAL_GPIOA_MUXL 0
-#define VAL_GPIOA_MUXH 0
-#define VAL_GPIOA_HDRV 0
+#define VAL_GPIOA_CFGR              (PIN_MODE_INPUT(GPIOA_PIN0) |           \
+                                     PIN_MODE_INPUT(GPIOA_PIN1) |           \
+                                     PIN_MODE_INPUT(GPIOA_LED_CAPS) |       \
+                                     PIN_MODE_INPUT(GPIOA_PIN3) |           \
+                                     PIN_MODE_INPUT(GPIOA_PIN4) |           \
+                                     PIN_MODE_INPUT(GPIOA_PIN5) |           \
+                                     PIN_MODE_INPUT(GPIOA_PIN6) |           \
+                                     PIN_MODE_INPUT(GPIOA_PIN7) |           \
+                                     PIN_MODE_INPUT(GPIOA_PIN8) |           \
+                                     PIN_MODE_INPUT(GPIOA_PIN9) |           \
+                                     PIN_MODE_INPUT(GPIOA_PIN10) |          \
+                                     PIN_MODE_INPUT(GPIOA_PIN11) |          \
+                                     PIN_MODE_INPUT(GPIOA_PIN12) |          \
+                                     PIN_MODE_MUX(GPIOA_SWDIO) |            \
+                                     PIN_MODE_MUX(GPIOA_SWCLK) |            \
+                                     PIN_MODE_INPUT(GPIOA_PIN15))
+#define VAL_GPIOA_OMODE             0U
+#define VAL_GPIOA_ODRVR             0U
+#define VAL_GPIOA_PULL              (PIN_PULL_PULLUP(GPIOA_PIN0) |          \
+                                     PIN_PULL_PULLUP(GPIOA_PIN1) |          \
+                                     PIN_PULL_PULLUP(GPIOA_LED_CAPS) |      \
+                                     PIN_PULL_PULLUP(GPIOA_PIN3) |          \
+                                     PIN_PULL_PULLUP(GPIOA_PIN4) |          \
+                                     PIN_PULL_PULLUP(GPIOA_PIN5) |          \
+                                     PIN_PULL_PULLUP(GPIOA_PIN6) |          \
+                                     PIN_PULL_PULLUP(GPIOA_PIN7) |          \
+                                     PIN_PULL_PULLUP(GPIOA_PIN8) |          \
+                                     PIN_PULL_PULLUP(GPIOA_PIN9) |          \
+                                     PIN_PULL_PULLUP(GPIOA_PIN10) |         \
+                                     PIN_PULL_FLOATING(GPIOA_PIN11) |       \
+                                     PIN_PULL_FLOATING(GPIOA_PIN12) |       \
+                                     PIN_PULL_PULLUP(GPIOA_SWDIO) |         \
+                                     PIN_PULL_PULLDOWN(GPIOA_SWCLK) |       \
+                                     PIN_PULL_PULLUP(GPIOA_PIN15))
+#define VAL_GPIOA_ODT               0xFFFFFFFFU
+#define VAL_GPIOA_MUXL              0U
+#define VAL_GPIOA_MUXH              0U
+#define VAL_GPIOA_HDRV              0U
 
-#define VAL_GPIOB_CFGR (PIN_MODE_INPUT(GPIOB_PIN0) | PIN_MODE_INPUT(GPIOB_PIN1) | \
-  PIN_MODE_INPUT(GPIOB_PIN2) | PIN_MODE_INPUT(GPIOB_PIN3) | PIN_MODE_INPUT(GPIOB_PIN4) | \
-  PIN_MODE_INPUT(GPIOB_PIN5) | PIN_MODE_INPUT(GPIOB_PIN6) | PIN_MODE_INPUT(GPIOB_PIN7) | \
-  PIN_MODE_INPUT(GPIOB_PIN8) | PIN_MODE_INPUT(GPIOB_PIN9) | PIN_MODE_INPUT(GPIOB_PIN10) | \
-  PIN_MODE_INPUT(GPIOB_PIN11) | PIN_MODE_INPUT(GPIOB_PIN12) | PIN_MODE_INPUT(GPIOB_USB_VBUS) | \
-  PIN_MODE_INPUT(GPIOB_PIN14) | PIN_MODE_INPUT(GPIOB_PIN15))
-#define VAL_GPIOB_OMODE 0
-#define VAL_GPIOB_ODRVR 0
-#define VAL_GPIOB_PULL (PIN_PULL_PULLUP(GPIOB_PIN0) | PIN_PULL_PULLUP(GPIOB_PIN1) | \
-  PIN_PULL_PULLUP(GPIOB_PIN2) | PIN_PULL_PULLUP(GPIOB_PIN3) | PIN_PULL_PULLUP(GPIOB_PIN4) | \
-  PIN_PULL_PULLUP(GPIOB_PIN5) | PIN_PULL_PULLUP(GPIOB_PIN6) | PIN_PULL_PULLUP(GPIOB_PIN7) | \
-  PIN_PULL_PULLUP(GPIOB_PIN8) | PIN_PULL_PULLUP(GPIOB_PIN9) | PIN_PULL_PULLUP(GPIOB_PIN10) | \
-  PIN_PULL_PULLUP(GPIOB_PIN11) | PIN_PULL_PULLUP(GPIOB_PIN12) | PIN_PULL_FLOATING(GPIOB_USB_VBUS) | \
-  PIN_PULL_PULLUP(GPIOB_PIN14) | PIN_PULL_PULLUP(GPIOB_PIN15))
-#define VAL_GPIOB_ODT 0xFFFFFFFFU
-#define VAL_GPIOB_MUXL 0
-#define VAL_GPIOB_MUXH 0
-#define VAL_GPIOB_HDRV 0
+/*
+ * Port B setup. All pins input pull-up except PB13 (VBUS, floating).
+ * AT32F405RC LQFP64 does not have PB14/PB15.
+ */
+#define VAL_GPIOB_CFGR              (PIN_MODE_INPUT(GPIOB_PIN0) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN1) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN2) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN3) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN4) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN5) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN6) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN7) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN8) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN9) |           \
+                                     PIN_MODE_INPUT(GPIOB_PIN10) |          \
+                                     PIN_MODE_INPUT(GPIOB_PIN11) |          \
+                                     PIN_MODE_INPUT(GPIOB_PIN12) |          \
+                                     PIN_MODE_INPUT(GPIOB_USB_VBUS))
+#define VAL_GPIOB_OMODE             0U
+#define VAL_GPIOB_ODRVR             0U
+#define VAL_GPIOB_PULL              (PIN_PULL_PULLUP(GPIOB_PIN0) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN1) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN2) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN3) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN4) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN5) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN6) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN7) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN8) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN9) |          \
+                                     PIN_PULL_PULLUP(GPIOB_PIN10) |         \
+                                     PIN_PULL_PULLUP(GPIOB_PIN11) |         \
+                                     PIN_PULL_PULLUP(GPIOB_PIN12) |         \
+                                     PIN_PULL_FLOATING(GPIOB_USB_VBUS))
+#define VAL_GPIOB_ODT               0xFFFFFFFFU
+#define VAL_GPIOB_MUXL              0U
+#define VAL_GPIOB_MUXH              0U
+#define VAL_GPIOB_HDRV              0U
 
-#define VAL_GPIOC_CFGR 0
-#define VAL_GPIOC_OMODE 0
-#define VAL_GPIOC_ODRVR 0
-#define VAL_GPIOC_PULL 0xFFFFFFFFU
-#define VAL_GPIOC_ODT 0xFFFFFFFFU
-#define VAL_GPIOC_MUXL 0
-#define VAL_GPIOC_MUXH 0
-#define VAL_GPIOC_HDRV 0
+/*
+ * Port C setup. All pins input pull-up.
+ * Pins PC12/PC11/PC10/PC5/PC3/PC2 are matrix row outputs (reconfigured by QMK).
+ */
+#define VAL_GPIOC_CFGR              0U
+#define VAL_GPIOC_OMODE             0U
+#define VAL_GPIOC_ODRVR             0U
+#define VAL_GPIOC_PULL              (PIN_PULL_PULLUP(GPIOC_PIN0) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN1) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN2) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN3) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN4) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN5) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN6) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN7) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN8) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN9) |          \
+                                     PIN_PULL_PULLUP(GPIOC_PIN10) |         \
+                                     PIN_PULL_PULLUP(GPIOC_PIN11) |         \
+                                     PIN_PULL_PULLUP(GPIOC_PIN12) |         \
+                                     PIN_PULL_PULLUP(GPIOC_PIN13) |         \
+                                     PIN_PULL_PULLUP(GPIOC_PIN14) |         \
+                                     PIN_PULL_PULLUP(GPIOC_PIN15))
+#define VAL_GPIOC_ODT               0xFFFFFFFFU
+#define VAL_GPIOC_MUXL              0U
+#define VAL_GPIOC_MUXH              0U
+#define VAL_GPIOC_HDRV              0U
 
-#define VAL_GPIOD_CFGR (PIN_MODE_INPUT(GPIOD_PIN0) | PIN_MODE_INPUT(GPIOD_PIN1) | PIN_MODE_INPUT(GPIOD_LED_SCROLL))
-#define VAL_GPIOD_OMODE 0
-#define VAL_GPIOD_ODRVR 0
-#define VAL_GPIOD_PULL (PIN_PULL_PULLUP(GPIOD_PIN0) | PIN_PULL_PULLUP(GPIOD_PIN1) | PIN_PULL_PULLUP(GPIOD_LED_SCROLL))
-#define VAL_GPIOD_ODT 0xFFFFFFFFU
-#define VAL_GPIOD_MUXL 0
-#define VAL_GPIOD_MUXH 0
-#define VAL_GPIOD_HDRV 0
+/*
+ * Port D setup. PD2 is Scroll Lock LED (reconfigured by QMK).
+ */
+#define VAL_GPIOD_CFGR              (PIN_MODE_INPUT(GPIOD_PIN2))
+#define VAL_GPIOD_OMODE             0U
+#define VAL_GPIOD_ODRVR             0U
+#define VAL_GPIOD_PULL              (PIN_PULL_PULLUP(GPIOD_PIN2))
+#define VAL_GPIOD_ODT               (PIN_ODT_HIGH(GPIOD_PIN2))
+#define VAL_GPIOD_MUXL              0U
+#define VAL_GPIOD_MUXH              0U
+#define VAL_GPIOD_HDRV              0U
 
-#define VAL_GPIOF_CFGR (PIN_MODE_INPUT(GPIOF_HEXT_IN) | PIN_MODE_INPUT(GPIOF_HEXT_OUT))
-#define VAL_GPIOF_OMODE 0
-#define VAL_GPIOF_ODRVR 0
-#define VAL_GPIOF_PULL (PIN_PULL_FLOATING(GPIOF_HEXT_IN) | PIN_PULL_FLOATING(GPIOF_HEXT_OUT))
-#define VAL_GPIOF_ODT 0xFFFFFFFFU
-#define VAL_GPIOF_MUXL 0
-#define VAL_GPIOF_MUXH 0
-#define VAL_GPIOF_HDRV 0
+/*
+ * Port F setup. PF0/PF1 are HEXT crystal (floating).
+ */
+#define VAL_GPIOF_CFGR              (PIN_MODE_INPUT(GPIOF_HEXT_IN) |        \
+                                     PIN_MODE_INPUT(GPIOF_HEXT_OUT))
+#define VAL_GPIOF_OMODE             0U
+#define VAL_GPIOF_ODRVR             0U
+#define VAL_GPIOF_PULL              (PIN_PULL_FLOATING(GPIOF_HEXT_IN) |     \
+                                     PIN_PULL_FLOATING(GPIOF_HEXT_OUT))
+#define VAL_GPIOF_ODT               (PIN_ODT_HIGH(GPIOF_HEXT_IN) |          \
+                                     PIN_ODT_HIGH(GPIOF_HEXT_OUT))
+#define VAL_GPIOF_MUXL              0U
+#define VAL_GPIOF_MUXH              0U
+#define VAL_GPIOF_HDRV              0U
 
 #if !defined(_FROM_ASM_)
 #ifdef __cplusplus

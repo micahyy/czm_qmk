6d219bc919fd8f5e359502766aff223f89519959
Micah <45813290+micahyy@users.noreply.github.com>
2026-08-23 17:07:17 +0800

    Fix v2 encoder: move pin config to keyboard.json, remove duplicate defines from config.h

--- keyboards/czmao/gamer/v2/config.h
+++ keyboards/czmao/gamer/v2/config.h
@@ -3,8 +3,6 @@
 #define DYNAMIC_KEYMAP_LAYER_COUNT 8
 
 #ifdef ENCODER_ENABLE
-#    define ENCODERS_PAD_A        { A5 }
-#    define ENCODERS_PAD_B        { A6 }
 #    define ENCODER_RESOLUTIONS   { 2 }
 #    define ENCODERS_MATRIX_MAP   {{{4, 5}, {5, 5}}}
 #endif

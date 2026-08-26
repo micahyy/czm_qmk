690d9f5fe3458f5c03aa5bd2bb80f47e4e846de1
micahyy <micahyy@foxmail.com>
2026-08-26 13:27:23 +0800

    EC87: change split space KC_NO to KC_SPC (all three space positions send space)

--- keyboards/czmao/ec87/keymaps/via/keymap.c
+++ keyboards/czmao/ec87/keymaps/via/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_PGDN    ,
         KC_CAPS    , KC_A       , KC_S       , KC_D       , KC_F       , KC_G       , KC_H       , KC_J       , KC_K       , KC_L       , KC_SCLN    , KC_QUOT    , KC_NUHS    ,
         KC_LSFT    , KC_NUBS    , KC_Z       , KC_X       , KC_C       , KC_V       , KC_B       , KC_N       , KC_M       , KC_COMM    , KC_DOT     , KC_SLSH    , KC_RSFT    , MO(2)      , KC_UP      ,
-        KC_LCTL    , KC_LGUI    , KC_LALT    , KC_NO      , KC_SPC     , KC_NO      , KC_RALT    , KC_RGUI    , MO(1)      , KC_RCTL    ,
+        KC_LCTL    , KC_LGUI    , KC_LALT    , KC_SPC     , KC_SPC     , KC_SPC     , KC_RALT    , KC_RGUI    , MO(1)      , KC_RCTL    ,
         KC_LEFT    , KC_DOWN    , KC_RGHT    
     ),
 

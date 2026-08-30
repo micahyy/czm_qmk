fa4d7df8c9e108d8c1b68b2dc6c6dfa004feae99
Micah <45813290+micahyy@users.noreply.github.com>
2026-08-30 22:23:50 +0800

    2

--- keyboards/czmao/ec87/config.h
+++ keyboards/czmao/ec87/config.h
@@ -2,6 +2,10 @@
 
 #define USB_MAX_POWER_CONSUMPTION 500
 
+/* Force NKRO on by default (redundant with host.default.nkro in keyboard.json,
+   kept for compatibility with older QMK versions that still use FORCE_NKRO) */
+#define FORCE_NKRO
+
 /* Capacitive sensing matrix */
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 16
